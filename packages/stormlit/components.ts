b6e4eb5b5aadb5cb2b81549e22b15fe264bedd03
frodrish <frodrish@gmail.com>
2025-10-14 00:19:13 -0400

    Support for grid based on SQL

--- packages/stormlit/components.ts
+++ packages/stormlit/components.ts
@@ -1,2 +1,5 @@
 export { default as Col } from './components/Col.astro';
 export { default as Table } from './components/Table.astro';
+export { default as Grid } from './components/Grid.astro';
+export { default as Show } from './components/Show.astro';
+//export { default as AstroCol } from './components/AstroCol.astro';
